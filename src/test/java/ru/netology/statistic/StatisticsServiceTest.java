2171a499186c82ff7528a05276363bccb293ed36
Ярослав Черников <yaroslavcher@gmail.com>
2022-08-03 20:28:48 +0300

    ready for netology

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -9,10 +9,9 @@ public class StatisticsServiceTest {
     @Test
     void findMax() {
         StatisticsService service = new StatisticsService();
-
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
 
+        long expected = 12;
         long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
@@ -21,13 +20,12 @@ public class StatisticsServiceTest {
     @Test
     void findMaxInMiddle() {
         StatisticsService service = new StatisticsService();
-
         long[] incomesInBillions = {12, 5, 8, 4, 5, 23, 8, 6, 11, 11, 12};
-        long expected = 23;
 
+        long expected = 23;
         long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
     }
-    
+
 }
